d5729dcd69c49a31217a89774296ab27748130a9
nehalfating03 <nehalfating03@gmail.com>
2026-09-19 00:33:39 +0530

    Time: 16 ms (6.70%) | Memory: 21.1 MB (5.15%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0; 
+        map<int, int> freq;
 
-        for(int j = 0;j<nums.size();j++){
-            if(i<2 || nums[j]!=nums[i-2]){
-                nums[i]=nums[j];
+        for(int x : nums) {
+            freq[x]++;
+        }
+
+        int i = 0;
+
+        for(auto x : freq) {
+            int num = x.first;
+            int count = x.second;
+
+            if(count >= 2) {
+                nums[i] = num;
+                i++;
+
+                nums[i] = num;
+                i++;
+            }
+            else {
+                nums[i] = num;
                 i++;
             }
         }
+
         return i;
     }
 };
