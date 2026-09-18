29904f69155e450e43d98ebad95d21e8cbf167e5
nehalfating03 <nehalfating03@gmail.com>
2026-09-18 23:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-
         int j = -1;
-        for(int i = 0 ; i<n ;i++){
+
+        for(int i= 0; i<n ; i++){
             if(nums[i]==0){
                 j = i;
                 break;
@@ -13,13 +13,14 @@ public:
         if(j==-1){
             return;
         }
-
-        for(int i = j+1 ; i<n ; i++){
+        for(int i = j+1; i<n ;i++){
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
             }
             
+            
         }
+        
     }
 };
